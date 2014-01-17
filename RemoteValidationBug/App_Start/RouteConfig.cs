52d7dc5f55fe026d13160bc5cd64de6de5d3d2b1
Vasile Postolache <vaioky@mail.ru>
2014-01-17 11:27:04 +0200

    Commit

---
+++ RemoteValidationBug/App_Start/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace RemoteValidationBug
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                constraints: null,
+                namespaces: new[] { "PQS.Web.CADS.Controllers" }
+            );
+        }
+    }
+}
---
+++ RemoteValidationBug/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RemoteValidationBug.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Save(SaveModel model)
+        {
+            if (model.PostAction == null)
+            {
+                throw new NullReferenceException("PostAction");
+            }
+            return null;
+        }
+
+        public JsonResult Remote(string name)
+        {
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class SaveModel
+    {
+        public string Name { get; set; }
+
+        public string PostAction { get; set; }
+    }
+}
